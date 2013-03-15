7f5bf735d59930cc5dcffb73347eed5458e94663
Aurélien Gâteau <agateau@kde.org>
2013-03-15 15:14:57 +0100

    Helper function to set button size

--- burgerparty/src/com/agateau/burgerparty/utils/UiUtils.java
+++ burgerparty/src/com/agateau/burgerparty/utils/UiUtils.java
@@ -1,6 +1,12 @@
 package com.agateau.burgerparty.utils;
 
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
 public class UiUtils {
 	public static final int BUTTON_WIDTH = 200;
 	public static final int BUTTON_HEIGHT = 40;
+
+	public static void setButtonSize(Actor actor) {
+		actor.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+	}
 }
