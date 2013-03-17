bd63af30f5c3b89470c7c64b8e3cda2a2da3324d
Aurélien Gâteau <agateau@kde.org>
2013-03-17 22:17:48 +0100

    More than one customer image

--- burgerparty/src/com/agateau/burgerparty/view/Customer.java
+++ burgerparty/src/com/agateau/burgerparty/view/Customer.java
@@ -3,12 +3,15 @@ package com.agateau.burgerparty.view;
 import com.agateau.burgerparty.utils.UiUtils;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class Customer extends Image {
 	public Customer(TextureAtlas atlas) {
-		TextureRegion region = atlas.findRegion("customer");
+		String names[] = {"a", "b"};
+		String name = names[MathUtils.random(0, 1)];
+		TextureRegion region = atlas.findRegion("customers/" + name);
 		setDrawable(new TextureRegionDrawable(region));
 		UiUtils.adjustToPrefSize(this);
 	}
