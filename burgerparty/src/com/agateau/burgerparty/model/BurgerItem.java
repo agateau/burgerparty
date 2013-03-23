9dff7713d24ec3f6b7b529e62b333d5415c823b8
Aurélien Gâteau <agateau@kde.org>
2013-03-23 01:29:12 +0100

    cleanup

--- burgerparty/src/com/agateau/burgerparty/model/BurgerItem.java
+++ burgerparty/src/com/agateau/burgerparty/model/BurgerItem.java
@@ -2,7 +2,6 @@ package com.agateau.burgerparty.model;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.OrderedMap;
 
