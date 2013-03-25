5f0283e32a2ba2250d0bde93ad87bc50e98665ac
Aurélien Gâteau <agateau@kde.org>
2013-03-25 10:31:15 +0100

    Rebase GameScreen on top of BaseScreen

--- burgerparty/src/com/agateau/burgerparty/screens/GameScreen.java
+++ burgerparty/src/com/agateau/burgerparty/screens/GameScreen.java
@@ -5,64 +5,22 @@ import com.agateau.burgerparty.model.Level;
 import com.agateau.burgerparty.model.World;
 import com.agateau.burgerparty.view.WorldView;
 
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
-public class GameScreen implements Screen {
-	private Stage mStage;
+public class GameScreen extends BaseScreen {
 	private World mWorld;
 	private WorldView mWorldView;
 
 	public GameScreen(BurgerPartyGame game, Level level, TextureAtlas atlas, Skin skin) {
-		mStage = new Stage(0, 0, true);
+		super(game, skin);
 		mWorld = new World(level);
 		mWorldView = new WorldView(game, mWorld, atlas, skin);
-		mStage.addActor(mWorldView);
-		Gdx.input.setInputProcessor(mStage);
-	}
-
-	@Override
-	public void render(float delta) {
-		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		mStage.act(delta);
-		mStage.draw();
-	}
-
-	@Override
-	public void resize(int width, int height) {
-		mStage.setViewport(width, height, true);
+		getStage().addActor(mWorldView);
 	}
 
 	@Override
 	public void show() {
 		mWorld.start();
 	}
-
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void pause() {
-		mWorldView.pause();
-
-	}
-
-	@Override
-	public void resume() {
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-
-	}
-
 }
