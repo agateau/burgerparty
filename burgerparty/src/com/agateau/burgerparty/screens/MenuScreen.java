24b831af6b5e29a4e32fe14a796d6500ad6c7b5b
Aurélien Gâteau <agateau@kde.org>
2013-03-25 10:28:42 +0100

    Rebase MenuScreen on BaseScreen

--- burgerparty/src/com/agateau/burgerparty/screens/MenuScreen.java
+++ burgerparty/src/com/agateau/burgerparty/screens/MenuScreen.java
@@ -5,74 +5,22 @@ import com.agateau.burgerparty.utils.Anchor;
 import com.agateau.burgerparty.utils.AnchorGroup;
 import com.agateau.burgerparty.utils.UiUtils;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
-public class MenuScreen implements Screen {
-	private BurgerPartyGame mGame;
-	private Stage mStage;
-
+public class MenuScreen extends BaseScreen {
 	public MenuScreen(BurgerPartyGame game, Skin skin) {
-		mGame = game;
-		mStage = new Stage(0, 0, true);
+		super(game, skin);
 		setupWidgets(skin);
-		Gdx.input.setInputProcessor(mStage);
-	}
-
-	@Override
-	public void render(float delta) {
-		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		mStage.act(delta);
-		mStage.draw();
-	}
-
-	@Override
-	public void resize(int width, int height) {
-		mStage.setViewport(width, height, true);
-	}
-
-	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-
 	}
 
 	private void setupWidgets(Skin skin) {
 		AnchorGroup group = new AnchorGroup();
 		group.setSpacing(UiUtils.SPACING);
-		mStage.addActor(group);
+		getStage().addActor(group);
 		group.setFillParent(true);
 
 		Label titleLabel = new Label("Burger Party", skin);
@@ -80,7 +28,7 @@ public class MenuScreen implements Screen {
 		TextButton startButton = new TextButton("Start", skin);
 		startButton.addListener(new ChangeListener() {
 			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
-				mGame.selectLevel();
+				getGame().selectLevel();
 			}
 		});
 		UiUtils.setButtonSize(startButton);
