d7c60b0a952149ea2d0f90600202bb5571204016
Aurélien Gâteau <agateau@kde.org>
2013-04-13 01:17:33 +0200

    Show all level groups

--- burgerparty/src/com/agateau/burgerparty/screens/LevelListScreen.java
+++ burgerparty/src/com/agateau/burgerparty/screens/LevelListScreen.java
@@ -9,19 +9,17 @@ import com.agateau.burgerparty.utils.TiledImage;
 import com.agateau.burgerparty.utils.UiUtils;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Array;
 
 public class LevelListScreen extends BaseScreen {
-	static private int COL_COUNT = 3;
-	private TextureRegion mStarOff;
-	private TextureRegion mStarOn;
-	private TextureRegion mLock;
-
 	public LevelListScreen(BurgerPartyGame game, TextureAtlas atlas, Skin skin) {
 		super(game, skin);
 		TiledImage bgImage = new TiledImage(atlas.findRegion("ui/menu-bg"));
@@ -34,33 +32,61 @@ public class LevelListScreen extends BaseScreen {
 	}
 
 	private void setupWidgets(Skin skin) {
-		AnchorGroup group = new AnchorGroup();
-		group.setSpacing(UiUtils.SPACING);
-		getStage().addActor(group);
-		group.setFillParent(true);
+		mAnchorGroup.setSpacing(UiUtils.SPACING);
+		getStage().addActor(mAnchorGroup);
+		mAnchorGroup.setFillParent(true);
 
 		TextButton backButton = new TextButton("<- Back", skin);
 		backButton.setSize(backButton.getPrefWidth(), UiUtils.BUTTON_HEIGHT);
-		group.addRule(backButton, Anchor.BOTTOM_LEFT, group, Anchor.BOTTOM_LEFT, 1, 1);
+		mAnchorGroup.addRule(backButton, Anchor.BOTTOM_LEFT, mAnchorGroup, Anchor.BOTTOM_LEFT, 1, 1);
 		backButton.addListener(new ChangeListener() {
 			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
 				getGame().showMenu();
 			}
 		});
 
+		mPreviousButton = new TextButton("<", skin);
+		mPreviousButton.setSize(UiUtils.BUTTON_HEIGHT, UiUtils.BUTTON_HEIGHT);
+		mPreviousButton.addListener(new ChangeListener() {
+			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
+				scrollTo(mGroupIndex - 1);
+			}
+		});
+
+		mNextButton = new TextButton(">", skin);
+		mNextButton.setSize(UiUtils.BUTTON_HEIGHT, UiUtils.BUTTON_HEIGHT);
+		mNextButton.addListener(new ChangeListener() {
+			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
+				scrollTo(mGroupIndex + 1);
+			}
+		});
+
+		for (int groupIndex = 0; groupIndex < getGame().getLevelGroupCount(); ++ groupIndex) {
+			GridGroup gridGroup = createLevelButtonGridGroup(groupIndex, skin);
+			gridGroup.setVisible(false);
+			mAnchorGroup.addActor(gridGroup);
+			mGridGroups.add(gridGroup);
+		}
+
+		// Add buttons after creating the grids so that buttons are above grids
+		mAnchorGroup.addRule(mPreviousButton, Anchor.CENTER_LEFT, mAnchorGroup, Anchor.CENTER_LEFT);
+		mAnchorGroup.addRule(mNextButton, Anchor.CENTER_RIGHT, mAnchorGroup, Anchor.CENTER_RIGHT);
+
+		scrollTo(0);
+	}
+
+	private GridGroup createLevelButtonGridGroup(int levelGroupIndex, Skin skin) {
 		GridGroup gridGroup = new GridGroup();
 		gridGroup.setSpacing(UiUtils.SPACING);
 		gridGroup.setColumnCount(COL_COUNT);
 		gridGroup.setCellSize(150, 150);
-		group.addRule(gridGroup, Anchor.TOP_CENTER, group, Anchor.TOP_CENTER, 0, -1);
 
-		// FIXME: Support multiple levels
-		int levelGroupIndex = 0;
 		LevelGroup levelGroup = getGame().getLevelGroup(levelGroupIndex);
 		for (int idx=0; idx < levelGroup.getLevelCount(); idx++) {
 			Actor levelButton = createLevelButton(levelGroupIndex, idx, skin);
 			gridGroup.addActor(levelButton);
 		}
+		return gridGroup;
 	}
 
 	class LevelButton extends TextButton {
@@ -106,4 +132,60 @@ public class LevelListScreen extends BaseScreen {
 
 		return button;
 	}
+
+	private void scrollTo(int index) {
+		assert(index >= 0);
+		assert(index < getGame().getLevelGroupCount());
+
+		GridGroup newGroup = mGridGroups.get(index);
+		newGroup.setVisible(true);
+		int oldIndex = mGroupIndex;
+		mGroupIndex = index;
+		if (oldIndex >= 0) {
+			float deltaX = getStage().getWidth() * (mGroupIndex < oldIndex ? 1 : -1);
+
+			GridGroup oldGroup = mGridGroups.get(oldIndex);
+			mAnchorGroup.removeRulesForActor(oldGroup);
+			oldGroup.addAction(Actions.moveBy(deltaX, 0, ANIMATION_DURATION, Interpolation.sineIn));
+
+			newGroup.setPosition(oldGroup.getX() - deltaX, oldGroup.getY());
+			newGroup.addAction(
+				Actions.sequence(
+					Actions.moveBy(deltaX, 0, ANIMATION_DURATION, Interpolation.sineIn),
+					Actions.run(new Runnable() {
+						@Override
+						public void run() {
+							setCurrentGridGroupAnchorRule();
+						}
+					})
+				)
+			);
+		} else {
+			setCurrentGridGroupAnchorRule();
+		}
+		updateButtons();
+	}
+
+	private void setCurrentGridGroupAnchorRule() {
+		mAnchorGroup.addRule(mGridGroups.get(mGroupIndex), Anchor.TOP_CENTER, mAnchorGroup, Anchor.TOP_CENTER, 0, -1);
+	}
+
+	private void updateButtons() {
+		mPreviousButton.setVisible(mGroupIndex > 0);
+		mNextButton.setVisible(mGroupIndex < getGame().getLevelGroupCount() - 1);
+	}
+
+	static private float ANIMATION_DURATION = 0.4f;
+	static private int COL_COUNT = 3;
+	private TextureRegion mStarOff;
+	private TextureRegion mStarOn;
+	private TextureRegion mLock;
+
+	private AnchorGroup mAnchorGroup = new AnchorGroup();
+	private Array<GridGroup> mGridGroups = new Array<GridGroup>();
+
+	private TextButton mPreviousButton;
+	private TextButton mNextButton;
+
+	private int mGroupIndex = -1;
 }
