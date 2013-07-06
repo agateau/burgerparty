a5280ffe07ec4c221e05bcf87fbb037eadba6c6f
Aurélien Gâteau <agateau@kde.org>
2013-07-06 21:57:10 +0200

    Use RoundButton

--- burgerparty/src/com/agateau/burgerparty/view/PauseOverlay.java
+++ burgerparty/src/com/agateau/burgerparty/view/PauseOverlay.java
@@ -3,12 +3,12 @@ package com.agateau.burgerparty.view;
 import com.agateau.burgerparty.BurgerPartyGame;
 import com.agateau.burgerparty.utils.Anchor;
 import com.agateau.burgerparty.utils.AnchorGroup;
+import com.agateau.burgerparty.utils.RoundButton;
 import com.agateau.burgerparty.utils.UiUtils;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class PauseOverlay extends Overlay {
@@ -22,39 +22,35 @@ public class PauseOverlay extends Overlay {
 
 		Label label = new Label("Paused", skin);
 
-		TextButton resumeButton = new TextButton("Resume", skin);
+		RoundButton resumeButton = new RoundButton(skin, "ui/icon-play");
 		resumeButton.addListener(new ChangeListener() {
 			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
 				mWorldView.resume();
 			}
 		});
 
-		TextButton restartButton = new TextButton("Restart", skin);
+		RoundButton restartButton = new RoundButton(skin, "ui/icon-restart");
 		restartButton.addListener(new ChangeListener() {
 			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
 				mGame.startLevel(mGame.getLevelWorldIndex(), mGame.getLevelIndex());
 			}
 		});
 
-		TextButton selectLevelButton = new TextButton("Levels", skin);
+		RoundButton selectLevelButton = new RoundButton(skin, "ui/icon-levels");
 		selectLevelButton.addListener(new ChangeListener() {
 			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
 				mGame.selectLevel();
 			}
 		});
 
-		UiUtils.setButtonSize(resumeButton);
-		UiUtils.setButtonSize(restartButton);
-		UiUtils.setButtonSize(selectLevelButton);
-
 		AnchorGroup group = new AnchorGroup();
 		addActor(group);
 		group.setFillParent(true);
 		group.setSpacing(UiUtils.SPACING);
 
-		group.addRule(label, Anchor.BOTTOM_CENTER, this, Anchor.CENTER, 0, 2);
-		group.addRule(resumeButton, Anchor.TOP_CENTER, this, Anchor.CENTER);
-		group.addRule(restartButton, Anchor.TOP_CENTER, resumeButton, Anchor.BOTTOM_CENTER, 0, -1);
-		group.addRule(selectLevelButton, Anchor.TOP_CENTER, restartButton, Anchor.BOTTOM_CENTER, 0, -1);
+		group.addRule(resumeButton, Anchor.CENTER, this, Anchor.CENTER);
+		group.addRule(label, Anchor.BOTTOM_CENTER, resumeButton, Anchor.TOP_CENTER, 0, 2);
+		group.addRule(restartButton, Anchor.BOTTOM_RIGHT, this, Anchor.BOTTOM_CENTER, -0.5f, 1);
+		group.addRule(selectLevelButton, Anchor.BOTTOM_LEFT, this, Anchor.BOTTOM_CENTER, 0.5f, 1);
 	}
 }
