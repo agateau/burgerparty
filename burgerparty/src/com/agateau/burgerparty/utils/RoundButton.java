00a6159d0b4feea0ad99c6c591ff98af928aa6f8
Aurélien Gâteau <agateau@kde.org>
2013-07-06 23:48:18 +0200

    Add sound support

--- burgerparty/src/com/agateau/burgerparty/utils/RoundButton.java
+++ burgerparty/src/com/agateau/burgerparty/utils/RoundButton.java
@@ -1,11 +1,21 @@
 package com.agateau.burgerparty.utils;
 
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class RoundButton extends ImageButton {
 	public RoundButton(Skin skin, String name) {
 		super(createStyle(skin, name));
+		addListener(new ChangeListener() {
+			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
+				if (mSound != null) {
+					mSound.play();
+				}
+			}
+		});
 	}
 
 	private static ImageButton.ImageButtonStyle createStyle(Skin skin, String name) {
@@ -14,4 +24,10 @@ public class RoundButton extends ImageButton {
 		style.imageUp = skin.getDrawable(name);
 		return style;
 	}
+
+	public void setSound(Sound sound) {
+		mSound = sound;
+	}
+
+	private Sound mSound = null;
 }
