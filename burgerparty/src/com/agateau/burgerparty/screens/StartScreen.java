d44c251b57ad412f084949f13b76b30dc0874594
Aurélien Gâteau <agateau@kde.org>
2013-09-24 17:23:23 +0200

    Use ImageTextButton for start screen

--- burgerparty/src/com/agateau/burgerparty/screens/StartScreen.java
+++ burgerparty/src/com/agateau/burgerparty/screens/StartScreen.java
@@ -4,13 +4,13 @@ import com.agateau.burgerparty.BurgerPartyGame;
 import com.agateau.burgerparty.Kernel;
 import com.agateau.burgerparty.utils.Anchor;
 import com.agateau.burgerparty.utils.AnchorGroup;
-import com.agateau.burgerparty.utils.RoundButton;
 import com.agateau.burgerparty.utils.UiUtils;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
@@ -31,23 +31,23 @@ public class StartScreen extends BurgerPartyScreen {
 
 		Image titleImage = new Image(atlas.findRegion("ui/title"));
 
-		RoundButton normalStartButton = Kernel.createRoundButton("ui/icon-play");
+		ImageTextButton normalStartButton = Kernel.createTextButton("Start", "ui/icon-play");
 		normalStartButton.addListener(new ChangeListener() {
 			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
 				getGame().selectLevel(0);
 			}
 		});
 
-		RoundButton sandBoxStartButton = Kernel.createRoundButton("ui/icon-play");
+		ImageTextButton sandBoxStartButton = Kernel.createTextButton("Sand Box", "ui/icon-play");
 		sandBoxStartButton.addListener(new ChangeListener() {
 			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
 				getGame().startSandBox();
 			}
 		});
 
-		group.addRule(titleImage, Anchor.CENTER, group, Anchor.CENTER, 0, 2);
-		group.addRule(normalStartButton, Anchor.TOP_CENTER, titleImage, Anchor.BOTTOM_CENTER, -2, 0);
-		group.addRule(sandBoxStartButton, Anchor.TOP_CENTER, titleImage, Anchor.BOTTOM_CENTER, 2, 0);
+		group.addRule(titleImage, Anchor.TOP_CENTER, group, Anchor.TOP_CENTER, 0, -0.5f);
+		group.addRule(normalStartButton, Anchor.TOP_CENTER, titleImage, Anchor.BOTTOM_CENTER, 0, 0.5f);
+		group.addRule(sandBoxStartButton, Anchor.TOP_CENTER, normalStartButton, Anchor.BOTTOM_CENTER, 0, -0.5f);
 	}
 
 	@Override
