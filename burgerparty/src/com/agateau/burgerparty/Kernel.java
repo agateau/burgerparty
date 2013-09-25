446e0ee92ba4b2147f32d6cde934d53c1fe736fa
Aurélien Gâteau <agateau@kde.org>
2013-09-25 10:22:11 +0200

    Factorize click sound loading

--- burgerparty/src/com/agateau/burgerparty/Kernel.java
+++ burgerparty/src/com/agateau/burgerparty/Kernel.java
@@ -4,6 +4,7 @@ import com.agateau.burgerparty.utils.AnimScriptLoader;
 import com.agateau.burgerparty.utils.RoundButton;
 import com.agateau.burgerparty.utils.SoundAtlas;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
@@ -34,19 +35,17 @@ public class Kernel {
 	public static RoundButton createRoundButton(String name) {
 		init();
 		RoundButton button = new RoundButton(sSkin, name);
-		button.setSound(sSoundAtlas.findSound("click"));
+		button.setSound(sClickSound);
 		return button;
 	}
 
 	public static ImageTextButton createTextButton(String text, String iconName) {
 		init();
 		ImageTextButton button = new ImageTextButton(text, sSkin, "image-text-button");
-		button.getImage().setDrawable(Kernel.getSkin().getDrawable(iconName));
-		button.addListener(new ChangeListener() {
-			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
-				sSoundAtlas.findSound("click").play();
-			}
-		});
+		button.getImage().setDrawable(sSkin.getDrawable(iconName));
+		button.addListener(sClickListener);
+		return button;
+	}
 
 		return button;
 	}
@@ -78,6 +77,13 @@ public class Kernel {
 			"time-bonus.wav"
 		};
 		sSoundAtlas.load(names);
+
+		sClickSound = sSoundAtlas.findSound("click");
+		sClickListener = new ChangeListener() {
+			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
+				sClickSound.play();
+			}
+		};
 	}
 
 	private static boolean sInited = false;
@@ -85,4 +91,6 @@ public class Kernel {
 	private static SoundAtlas sSoundAtlas = new SoundAtlas("sounds/");
 	private static TextureAtlas sTextureAtlas;
 	private static Skin sSkin;
+	private static Sound sClickSound;
+	private static ChangeListener sClickListener;
 }
