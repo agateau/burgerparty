f135b69b6c3078e935e22e3f714c50cb16f137d5
Aurélien Gâteau <agateau@kde.org>
2013-09-25 10:22:56 +0200

    Start creating bottom left bar

--- burgerparty/src/com/agateau/burgerparty/view/SandBoxGameView.java
+++ burgerparty/src/com/agateau/burgerparty/view/SandBoxGameView.java
@@ -10,13 +10,17 @@ import com.agateau.burgerparty.model.MealItem;
 import com.agateau.burgerparty.model.SandBoxWorld;
 import com.agateau.burgerparty.screens.BurgerPartyScreen;
 import com.agateau.burgerparty.utils.Anchor;
+import com.agateau.burgerparty.utils.AnchorGroup;
 import com.agateau.burgerparty.utils.Signal1;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
 
 public class SandBoxGameView extends AbstractWorldView {
@@ -27,7 +31,9 @@ public class SandBoxGameView extends AbstractWorldView {
 		mGame = game;
 
 		setupWidgets();
+		setupBottomLeftBar();
 		setupInventory();
+		setLevelWorldIndex(0);
 		Gdx.app.postRunnable(new Runnable() {
 			@Override
 			public void run() {
@@ -48,13 +54,6 @@ public class SandBoxGameView extends AbstractWorldView {
 			}
 		});
 
-		ImageButton switchInventoriesButton = Kernel.createRoundButton("ui/icon-back");
-		switchInventoriesButton.addListener(new ChangeListener() {
-			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
-				switchInventories();
-			}
-		});
-
 		ImageButton worldButton = Kernel.createRoundButton("ui/icon-levels");
 		worldButton.addListener(new ChangeListener() {
 			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
@@ -69,18 +68,35 @@ public class SandBoxGameView extends AbstractWorldView {
 			}
 		});
 
-		ImageButton undoButton = Kernel.createRoundButton("ui/icon-right");
+		addRule(backButton, Anchor.TOP_LEFT, this, Anchor.TOP_LEFT);
+		addRule(worldButton, Anchor.CENTER_LEFT, backButton, Anchor.CENTER_RIGHT, 1, 0);
+		addRule(deliverButton, Anchor.CENTER_RIGHT, this, Anchor.CENTER_RIGHT);
+	}
+
+	private void setupBottomLeftBar() {
+		ImageButton switchInventoriesButton = Kernel.createHudButton("ui/icon-back");
+		switchInventoriesButton.setSize(48, 46);
+		switchInventoriesButton.addListener(new ChangeListener() {
+			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
+				switchInventories();
+			}
+		});
+
+		ImageButton undoButton = Kernel.createHudButton("ui/icon-right");
+		undoButton.setSize(48, 46);
 		undoButton.addListener(new ChangeListener() {
 			public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
 				undo();
 			}
 		});
 
-		addRule(backButton, Anchor.TOP_LEFT, this, Anchor.TOP_LEFT);
-		addRule(worldButton, Anchor.CENTER_LEFT, backButton, Anchor.CENTER_RIGHT, 1, 0);
-		addRule(switchInventoriesButton, Anchor.CENTER_LEFT, this, Anchor.CENTER_LEFT);
-		addRule(undoButton, Anchor.BOTTOM_LEFT, switchInventoriesButton, Anchor.TOP_LEFT, 0, 1);
-		addRule(deliverButton, Anchor.CENTER_RIGHT, this, Anchor.CENTER_RIGHT);
+		mBottomLeftBgImage.setFillParent(true);
+		mBottomLeftBar.addActor(mBottomLeftBgImage);
+
+		mBottomLeftBar.addRule(switchInventoriesButton, Anchor.BOTTOM_LEFT, mBottomLeftBar, Anchor.BOTTOM_LEFT, 1, 1);
+		mBottomLeftBar.addRule(undoButton, Anchor.BOTTOM_LEFT, switchInventoriesButton, Anchor.TOP_LEFT, 1, 3);
+
+		addRule(mBottomLeftBar, Anchor.BOTTOM_LEFT, mInventoryView, Anchor.TOP_LEFT);
 	}
 
 	private void setupInventory() {
@@ -157,7 +173,12 @@ public class SandBoxGameView extends AbstractWorldView {
 	private void setLevelWorldIndex(int index) {
 		mLevelWorldIndex = index;
 		LevelWorld levelWorld = mGame.getLevelWorld(index);
-		setWorldDirName(levelWorld.getDirName());
+		String dirName = levelWorld.getDirName();
+		setWorldDirName(dirName);
+
+		TextureRegion region = Kernel.getTextureAtlas().findRegion(dirName + "bottom-left-button-bar");
+		mBottomLeftBgImage.setDrawable(new TextureRegionDrawable(region));
+		mBottomLeftBar.setSize(region.getRegionWidth(), region.getRegionHeight());
 	}
 
 	private void onAddItem(MealItem item) {
@@ -175,4 +196,7 @@ public class SandBoxGameView extends AbstractWorldView {
 	private MealView mMealView;
 	private final BurgerPartyGame mGame;
 	private final BurgerPartyScreen mScreen;
+
+	Image mBottomLeftBgImage = new Image();
+	AnchorGroup mBottomLeftBar = new AnchorGroup();
 }
