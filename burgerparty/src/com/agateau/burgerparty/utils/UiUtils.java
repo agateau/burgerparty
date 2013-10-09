288f6b3f11255bbfefc662bdfcb4259274fcd441
Aurélien Gâteau <agateau@kde.org>
2013-10-09 12:26:55 +0200

    New method to convert coordinates, with unit tests

--- burgerparty/src/com/agateau/burgerparty/utils/UiUtils.java
+++ burgerparty/src/com/agateau/burgerparty/utils/UiUtils.java
@@ -1,5 +1,6 @@
 package com.agateau.burgerparty.utils;
 
+import com.badlogic.gdx.math.Matrix3;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Widget;
@@ -33,17 +34,37 @@ public class UiUtils {
 	/**
 	 * Same as Actor.toAscendantCoordinates(), but supports scaled actors
 	 */
-	public static Vector2 toAscendantCoordinates(Actor ascendant, Actor _actor, Vector2 _coords) {
-		Vector2 coords = new Vector2(_coords);
-		Actor actor = _actor;
-		while (actor != ascendant) {
-			coords.x = actor.getX() + coords.x * actor.getScaleX();
-			coords.y = actor.getY() + coords.y * actor.getScaleY();
-			actor = actor.getParent();
-			if (actor == null) {
-				throw new RuntimeException("actor " + ascendant + " is not an ascendant of actor " + _actor);
+	public static Vector2 toAscendantCoordinates(Actor ascendant, Actor actor, Vector2 coords) {
+		Matrix3 matrix = getAscendantMatrix(ascendant, actor);
+		return coords.mul(matrix);
+	}
+
+	public static Vector2 toChildCoordinates(Actor ascendant, Actor actor, Vector2 coords) {
+		Matrix3 matrix = getAscendantMatrix(ascendant, actor);
+		return coords.mul(matrix.inv());
+	}
+
+	/**
+	 * Returns the matrix to transform @p actor coordinates into @p ascendant coordinates
+	 */
+	public static Matrix3 getAscendantMatrix(Actor ascendant, Actor actor) {
+		Matrix3 matrix = new Matrix3();
+		Actor actor2 = actor;
+		while (actor2 != ascendant) {
+			matrix = getActorMatrix(actor2).mul(matrix);
+			actor2 = actor2.getParent();
+			if (actor2 == null) {
+				throw new RuntimeException("actor " + ascendant + " is not an ascendant of actor " + actor);
 			}
 		}
-		return coords;
+		return matrix;
+	}
+
+	public static Matrix3 getActorMatrix(Actor actor) {
+		Matrix3 matrix = new Matrix3();
+		matrix.translate(actor.getX(), actor.getY());
+		matrix.scale(actor.getScaleX(), actor.getScaleY());
+		matrix.rotate(actor.getRotation());
+		return matrix;
 	}
 }
