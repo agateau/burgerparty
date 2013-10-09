60ac76db44d5580aa72f2ff6b2e23366c23ac607
Aurélien Gâteau <agateau@kde.org>
2013-10-09 17:59:29 +0200

    Add gradient edges to scroll pane

--- burgerparty/src/com/agateau/burgerparty/view/MealViewScrollPane.java
+++ burgerparty/src/com/agateau/burgerparty/view/MealViewScrollPane.java
@@ -2,14 +2,19 @@ package com.agateau.burgerparty.view;
 
 import java.util.HashSet;
 
+import com.agateau.burgerparty.Kernel;
 import com.agateau.burgerparty.model.Burger;
 import com.agateau.burgerparty.utils.ResizeToFitChildren;
 import com.agateau.burgerparty.utils.Signal1;
 import com.agateau.burgerparty.utils.UiUtils;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 
 class MealViewScrollPane extends ScrollPane implements ResizeToFitChildren {
+	private static final float EDGE_SIZE = 20f;
+
 	public MealViewScrollPane(MealView child) {
 		super(child);
 		mMealView = child;
@@ -31,6 +36,12 @@ class MealViewScrollPane extends ScrollPane implements ResizeToFitChildren {
 	@Override
 	public void onChildSizeChanged() {
 		updateSize();
+	}
+
+	@Override
+	public void layout() {
+		super.layout();
+		updateEdgeImages();
 	}
 
 	private void updateSize() {
@@ -58,8 +69,43 @@ class MealViewScrollPane extends ScrollPane implements ResizeToFitChildren {
 		setScrollY(mMealView.getHeight() - scrollY);
 	}
 
+	@Override
+	protected void visualScrollY(float pixelsY) {
+		super.visualScrollY(pixelsY);
+		updateEdgeImages();
+	}
+
+	private void updateEdgeImages() {
+		if (mTopEdge == null) {
+			// This is a bit hackish, but we can't add the edge images to the pane itself: it is forbidden to call ScrollPane.addActor()
+			TextureRegion region = Kernel.getTextureAtlas().findRegion("ui/vertical-gradient");
+			mTopEdge = new Image(region);
+			TextureRegion region2 = new TextureRegion(region);
+			region2.flip(false, true);
+			mBottomEdge = new Image(region2);
+			getParent().addActor(mTopEdge);
+			getParent().addActor(mBottomEdge);
+		}
+		mTopEdge.setVisible(false);
+		mBottomEdge.setVisible(false);
+		if (getWidget().getHeight() <= getHeight()) {
+			return;
+		}
+		float percent = getScrollPercentY();
+		if (percent < 1) {
+			mBottomEdge.setVisible(true);
+			mBottomEdge.setBounds(getX(), getY(), getWidth() * getScaleX(), EDGE_SIZE);
+		}
+		if (percent > 0) {
+			mTopEdge.setVisible(true);
+			mTopEdge.setBounds(getX(), getY() + getHeight() * getScaleY() - EDGE_SIZE, getWidth() * getScaleX(), EDGE_SIZE);
+		}
+	}
+
 	private HashSet<Object> mHandlers = new HashSet<Object>();
 
 	private float mMaximumHeight = 1000;
 	private MealView mMealView;
+	private Image mTopEdge;
+	private Image mBottomEdge;
 }
