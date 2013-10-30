32fefd6f9856d634082a0e87af68c08744a670ec
Aurélien Gâteau <agateau@kde.org>
2013-10-30 10:59:13 +0100

    Make LevelWorld independent of I/O

--- burgerparty/src/com/agateau/burgerparty/model/LevelWorld.java
+++ burgerparty/src/com/agateau/burgerparty/model/LevelWorld.java
@@ -1,26 +1,18 @@
 package com.agateau.burgerparty.model;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Array;
 
 public class LevelWorld {
 	public LevelWorld(String dirName) {
 		mDirName = dirName;
-
-		for (int n=1;; n++) {
-			String name = dirName + "/" + n + ".xml";
-			FileHandle levelFile = Gdx.files.internal(name);
-			if (!levelFile.exists()) {
-				break;
-			}
-			Gdx.app.log("LevelWorld", "levelFile=" + levelFile);
-			mLevels.add(Level.fromXml(this, levelFile));
-		}
 	}
 
 	public String getDirName() {
 		return mDirName;
+	}
+
+	public void addLevel(Level level) {
+		mLevels.add(level);
 	}
 
 	public Level getLevel(int index) {
--- burgerparty/src/com/agateau/burgerparty/model/LevelWorldLoader.java
+++ burgerparty/src/com/agateau/burgerparty/model/LevelWorldLoader.java
@@ -3,6 +3,7 @@ package com.agateau.burgerparty.model;
 import java.util.HashSet;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Array;
 
 public class LevelWorldLoader {
@@ -13,12 +14,26 @@ public class LevelWorldLoader {
 			if (!Gdx.files.internal(dirName + "1.xml").exists()) {
 				break;
 			}
-			Gdx.app.log("loadLevelWorlds", "dir=" + dirName);
-			LevelWorld world = new LevelWorld(dirName);
+			Gdx.app.log("LevelWorldLoader", "dir=" + dirName);
+			LevelWorld world = loadWorld(dirName);
 			worlds.add(world);
 		}
 		checkNewItems(worlds);
 		return worlds;
+	}
+
+	private LevelWorld loadWorld(String dirName) {
+		LevelWorld world = new LevelWorld(dirName);
+		for (int n=1;; n++) {
+			String name = dirName + "/" + n + ".xml";
+			FileHandle levelFile = Gdx.files.internal(name);
+			if (!levelFile.exists()) {
+				break;
+			}
+			Gdx.app.log("LevelWorldLoader", "levelFile=" + levelFile);
+			world.addLevel(Level.fromXml(world, levelFile));
+		}
+		return world;
 	}
 
 	private void checkNewItems(Array<LevelWorld> worlds) {
