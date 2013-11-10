a08817430a2ff87bf08ac48d47095a8c11d9119d
Aurélien Gâteau <agateau@kde.org>
2013-11-10 23:10:14 +0100

    Add ability to customize button sounds

--- burgerparty/src/com/agateau/burgerparty/view/BurgerPartyUiBuilder.java
+++ burgerparty/src/com/agateau/burgerparty/view/BurgerPartyUiBuilder.java
@@ -2,7 +2,10 @@ package com.agateau.burgerparty.view;
 
 import com.agateau.burgerparty.Assets;
 import com.agateau.burgerparty.utils.UiBuilder;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.XmlReader;
 
 public class BurgerPartyUiBuilder extends UiBuilder {
@@ -13,7 +16,21 @@ public class BurgerPartyUiBuilder extends UiBuilder {
 
 	protected ImageButton createImageButton(XmlReader.Element element) {
 		ImageButton button = super.createImageButton(element);
-		button.addListener(mAssets.getClickListener());
+		String soundName = element.getAttribute("sound", "");
+
+		ChangeListener listener;
+		if (soundName.isEmpty()) {
+			listener = mAssets.getClickListener();
+		} else {
+			final Sound sound = mAssets.getSoundAtlas().findSound(soundName);
+			listener = new ChangeListener() {
+				public void changed(ChangeListener.ChangeEvent Event, Actor actor) {
+					sound.play();
+				}
+			};
+		};
+		button.addListener(listener);
+
 		return button;
 	}
 
