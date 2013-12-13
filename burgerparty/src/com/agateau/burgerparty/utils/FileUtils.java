6e4ebf339d1046a95b859a6fc9c408741339eb38
Aurélien Gâteau <agateau@kde.org>
2013-12-13 15:58:47 +0100

    Use xdg standard dir to store progress

--- burgerparty/src/com/agateau/burgerparty/utils/FileUtils.java
+++ burgerparty/src/com/agateau/burgerparty/utils/FileUtils.java
@@ -8,7 +8,7 @@ public class FileUtils {
 	static public FileHandle getUserWritableFile(String name) {
 		FileHandle handle;
 		if (Gdx.app.getType() == ApplicationType.Desktop) {
-			handle = Gdx.files.external(".local/burgerparty/" + name);
+			handle = Gdx.files.external(".local/share/burgerparty/" + name);
 		} else {
 			handle = Gdx.files.local(name);
 		}
