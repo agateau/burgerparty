31f400925475b30c4e6a1da50275794e72e47a3b
Aurélien Gâteau <agateau@kde.org>
2013-12-22 18:35:45 +0100

    Add helper to refresh gdxui-based interface with F5

---
+++ burgerparty/src/com/agateau/burgerparty/utils/RefreshHelper.java
@@ -0,0 +1,32 @@
+package com.agateau.burgerparty.utils;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Timer;
+
+public abstract class RefreshHelper {
+	public RefreshHelper(Stage stage) {
+		stage.getRoot().addListener(new InputListener() {
+			@Override
+			public boolean keyUp(InputEvent event, int keycode) {
+				if (keycode == Input.Keys.F5) {
+					Timer.post(new Timer.Task() {
+						@Override
+						public void run() {
+							refresh();
+						}
+					});
+					return true;
+				}
+				return false;
+			}
+		});
+	}
+
+	/**
+	 * Implementation of this method must do the refresh
+	 */
+	protected abstract void refresh();
+}
