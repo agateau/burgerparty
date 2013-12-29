0ccbba46679e3485b6c12c957672e9284bd632d7
Aurélien Gâteau <agateau@kde.org>
2013-12-30 00:55:27 +0100

    Massive refactor

--- burgerparty/src/com/agateau/burgerparty/burgercopter/BurgerCopterMainScreen.java
+++ burgerparty/src/com/agateau/burgerparty/burgercopter/BurgerCopterMainScreen.java
@@ -1,9 +1,9 @@
 package com.agateau.burgerparty.burgercopter;
 
-import com.agateau.burgerparty.Assets;
 import com.agateau.burgerparty.utils.StageScreen;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.FPSLogger;
+import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Action;
@@ -12,14 +12,16 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.Array;
 
 public class BurgerCopterMainScreen extends StageScreen {
-	private static final float PIXEL_PER_SECOND = 600;
-	private static final int TILE_SIZE = 32;
+	static final float PIXEL_PER_SECOND = 180;
+	static final int TILE_SIZE = 32;
+	static final int ENEMY_COUNT = 4;
 	public BurgerCopterMainScreen(BurgerCopterMiniGame miniGame) {
 		super(miniGame.getAssets().getSkin());
 		mMiniGame = miniGame;
-		createSky();
+		//createSky();
 		//createBg();
 		createGround();
+		createEnemies();
 		createPlayer();
 	}
 
@@ -28,57 +30,21 @@ public class BurgerCopterMainScreen extends StageScreen {
 		mMiniGame.showStartScreen();
 	}
 
-	private static class GroundActor extends Actor {
-		public GroundActor(Assets assets) {
-			final TextureRegion groundRegion = assets.getTextureAtlas().findRegion("burgercopter/ground");
-			final TextureRegion buildingRegion = assets.getTextureAtlas().findRegion("burgercopter/building");
-
-			mGroundColumnCount = (int)(Gdx.graphics.getWidth()) / TILE_SIZE + 1;
-			mGroundRowCount = 8;
-			mGroundColumns = new Array<Array <TextureRegion>>(mGroundColumnCount);
-			for (int col = 0; col < mGroundColumnCount; ++col) {
-				Array<TextureRegion> column = new Array<TextureRegion>(mGroundRowCount);
-				column.add(groundRegion);
-				int rows = MathUtils.random(1, mGroundRowCount);
-				for (int row = 1; row < mGroundRowCount; ++row) {
-					if (row <= rows) {
-						column.add(buildingRegion);
-					} else {
-						column.add(null);
-					}
-				}
-				mGroundColumns.add(column);
+	private FPSLogger mLogger = new FPSLogger();
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0.8f, 0.95f, 1, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		if (mGameOverDelay < 0) {
+			getStage().act(delta);
+		} else {
+			mGameOverDelay += delta;
+			if (mGameOverDelay > 2) {
+				mMiniGame.showStartScreen();
 			}
 		}
-
-		@Override
-		public void act(float delta) {
-			mScrollOffset -= delta * PIXEL_PER_SECOND;
-			if (mScrollOffset < -TILE_SIZE) {
-				mScrollOffset = 0;
-				mStartCol = (mStartCol + 1) % mGroundColumnCount;
-			}
-		}
-
-		@Override
-		public void draw(SpriteBatch batch, float parentAlpha) {
-			int snapScrollOffset = (int)mScrollOffset;
-			for (int col = 0; col < mGroundColumnCount; ++col) {
-				for (int row = 0; row < mGroundRowCount; ++row) {
-					TextureRegion region = mGroundColumns.get((col + mStartCol) % mGroundColumnCount).get(row);
-					if (region == null) {
-						continue;
-					}
-					batch.draw(region, col * TILE_SIZE + snapScrollOffset, row * TILE_SIZE);
-				}
-			}
-		}
-
-		private int mGroundRowCount;
-		private int mGroundColumnCount;
-		private Array<Array<TextureRegion>> mGroundColumns;
-		private float mScrollOffset = 0;
-		private int mStartCol = 0;
+		getStage().draw();
+		mLogger.log();
 	}
 
 	private static class FollowMouseAction extends Action {
@@ -129,27 +95,28 @@ public class BurgerCopterMainScreen extends StageScreen {
 	}
 
 	private void createPlayer() {
-		TextureRegion region = mMiniGame.getAssets().getTextureAtlas().findRegion("mealitems/0/top-inventory");
+		TextureRegion region = mMiniGame.getAssets().getTextureAtlas().findRegion("mealitems/2/soda-inventory");
 		assert(region != null);
-		Image image = new Image(region);
+		SpriteImage image = new SpriteImage(region);
+		mPlayer = image;
 		image.setX(10);
-		image.setY(Gdx.graphics.getHeight() / 2);
+		image.setY(Gdx.graphics.getHeight() * 3 / 4);
 		image.addAction(new Action() {
 			@Override
 			public boolean act(float delta) {
-				Actor player = getActor();
-				/*for (Actor wall: mWalls) {
-					if (collide(player, wall)) {
-						// gameover
-						mMiniGame.showStartScreen();
+				if (mGroundActor.collide(mPlayer)) {
+					gameOver();
+				}
+				for(SpriteImage enemy: mEnemies) {
+					if (SpriteImage.collide(mPlayer, enemy)) {
+						gameOver();
 					}
-				}*/
+				}
 				return false;
 			}
 		});
 //		image.addAction(new FollowMouseAction());
 		image.addAction(new GravityAction());
-		mPlayer = image;
 		getStage().addActor(mPlayer);
 	}
 
@@ -173,40 +140,63 @@ public class BurgerCopterMainScreen extends StageScreen {
 	}
 
 	private void createGround() {
-		Actor actor = new GroundActor(mMiniGame.getAssets());
-		getStage().addActor(actor);
+		int columnCount = (int)(Gdx.graphics.getWidth()) / TILE_SIZE + 1;
+		int rowCount = 8;
+		mGroundActor = new TileActor(columnCount, rowCount, TILE_SIZE);
+
+		final TextureRegion groundRegion = mMiniGame.getAssets().getTextureAtlas().findRegion("burgercopter/ground");
+		final TextureRegion buildingRegion = mMiniGame.getAssets().getTextureAtlas().findRegion("burgercopter/building");
+
+		for (int col = 0; col < columnCount; ++col) {
+			Array<TextureRegion> column = mGroundActor.getColumn(col);
+			int rows = MathUtils.random(1, rowCount);
+			column.set(0, groundRegion);
+			for (int row = 1; row < rows; ++row) {
+				column.set(row, buildingRegion);
+			}
+		}
+
+		getStage().addActor(mGroundActor);
 	}
 
-	private static boolean collide(Actor a1, Actor a2) {
-		if (!a1.isVisible()) {
+	private static class EnemyAction extends ScrollAction {
+		public EnemyAction(float speed) {
+			super(speed);
+		}
+
+		@Override
+		public boolean onFinished() {
+			int x = Gdx.graphics.getWidth();
+			int y = MathUtils.random(240, 480);
+			actor.setX(x);
+			actor.setY(y);
 			return false;
 		}
-		if (!a2.isVisible()) {
-			return false;
+	}
+	private void createEnemies() {
+		final TextureRegion region = mMiniGame.getAssets().getTextureAtlas().findRegion("mealitems/0/fish-inventory");
+		assert(region != null);
+		SpriteImage.CollisionMask mask = new SpriteImage.CollisionMask(region);
+		float screenWidth = Gdx.graphics.getWidth();
+		for (int idx = 0; idx < ENEMY_COUNT; ++idx) {
+			float x = screenWidth * (1 + (float)idx / ENEMY_COUNT);
+			float y = MathUtils.random(240, 480);
+			SpriteImage actor = new SpriteImage(region, mask);
+			actor.setX(x);
+			actor.setY(y);
+			actor.addAction(new EnemyAction(PIXEL_PER_SECOND * 2));
+			getStage().addActor(actor);
+			mEnemies.add(actor);
 		}
-		float a1Left = a1.getX();
-		float a1Right = a1.getRight();
-		float a1Bottom = a1.getY();
-		float a1Top = a1.getTop();
-		float a2Left = a2.getX();
-		float a2Right = a2.getRight();
-		float a2Bottom = a2.getY();
-		float a2Top = a2.getTop();
-		if (a1Right < a2Left) {
-			return false;
-		}
-		if (a2Right < a1Left) {
-			return false;
-		}
-		if (a1Top < a2Bottom) {
-			return false;
-		}
-		if (a2Top < a1Bottom) {
-			return false;
-		}
-		return true;
 	}
 
+	private void gameOver() {
+		mGameOverDelay = 0;
+	}
+
+	private float mGameOverDelay = -1;
 	private BurgerCopterMiniGame mMiniGame;
-	private Actor mPlayer;
+	private SpriteImage mPlayer;
+	private TileActor mGroundActor;
+	private Array<SpriteImage> mEnemies = new Array<SpriteImage>();
 }
