bec17bbecd89979f2a41c66f0827318386163779
Aurélien Gâteau <agateau@kde.org>
2014-01-17 17:09:35 +0100

    deadcode--

--- burgerparty/src/com/agateau/burgerparty/Kernel.java
+++ burgerparty/src/com/agateau/burgerparty/Kernel.java
@@ -1,19 +1,11 @@
 package com.agateau.burgerparty;
 
 import com.agateau.burgerparty.utils.RoundButton;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
 
 public class Kernel {
 	public static RoundButton createRoundButton(Assets assets, String name) {
 		RoundButton button = new RoundButton(assets.getSkin(), name);
 		button.setSound(assets.getClickSound());
-		return button;
-	}
-
-	public static ImageTextButton createTextButton(Assets assets, String text, String iconName) {
-		ImageTextButton button = new ImageTextButton(text, assets.getSkin(), "image-text-button");
-		button.getImage().setDrawable(assets.getSkin().getDrawable(iconName));
-		button.addListener(assets.getClickListener());
 		return button;
 	}
 }
