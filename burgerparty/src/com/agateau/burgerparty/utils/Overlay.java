cf95f24425af0f136ad1cea6ad52376f696351b4
Aurélien Gâteau <agateau@kde.org>
2014-01-22 10:34:58 +0100

    Use ui/white-pixel for overlay backgrounds

--- burgerparty/src/com/agateau/burgerparty/utils/Overlay.java
+++ burgerparty/src/com/agateau/burgerparty/utils/Overlay.java
@@ -9,9 +9,10 @@ import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
 
 public abstract class Overlay extends WidgetGroup {
-	private TextureRegion mBackgroundRegion;
+	private static float ALPHA = 0.7f;
+
 	public Overlay(TextureAtlas atlas) {
-		mBackgroundRegion = atlas.findRegion("overlay-bg");
+		mBackgroundRegion = atlas.findRegion("ui/white-pixel");
 		setFillParent(true);
 
 		// Disable clicks behind us
@@ -27,12 +28,15 @@ public abstract class Overlay extends WidgetGroup {
 
 	@Override
 	public void draw(SpriteBatch batch, float parentAlpha) {
+		batch.setColor(0, 0, 0, ALPHA);
 		batch.draw(mBackgroundRegion, 0, 0, getWidth(), getHeight());
 		super.draw(batch, parentAlpha);
 	}
 
 	/**
-	 * Must be reimplemented to handle pressing the "back" button
+	 * Must be implemented to handle pressing the "back" button
 	 */
 	public abstract void onBackPressed();
+
+	private TextureRegion mBackgroundRegion;
 }
