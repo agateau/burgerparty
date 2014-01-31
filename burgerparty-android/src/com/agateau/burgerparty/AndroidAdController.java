49ab0d0258a47cfc5fb677c4ccc6ad6908401827
Aurélien Gâteau <agateau@kde.org>
2014-01-31 19:10:24 +0100

    Switch to Heyzap

--- burgerparty-android/src/com/agateau/burgerparty/AndroidAdController.java
+++ burgerparty-android/src/com/agateau/burgerparty/AndroidAdController.java
@@ -1,9 +1,10 @@
 package com.agateau.burgerparty;
 
 import com.agateau.burgerparty.utils.NLog;
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
+
+import com.heyzap.sdk.ads.HeyzapAds;
+import com.heyzap.sdk.ads.HeyzapAds.OnStatusListener;
+import com.heyzap.sdk.ads.InterstitialAd;
 
 import android.app.Activity;
 import android.os.Handler;
@@ -12,31 +13,48 @@ import android.os.Message;
 public class AndroidAdController implements AdController {
 	private static final int PRELOAD_MSG = 0;
 	private static final int SHOW_MSG = 1;
-	private static String INTERSTITIAL_ID = "ca-app-pub-1401038189223985/1605506750";
 
-	private class LogAdListener extends AdListener {
+	private class StatusListener implements OnStatusListener {
 		@Override
-		public void onAdClosed() {
-			log.i("onAdClosed");
-			preloadAd();
+		public void onShow(String tag) {
+			log.i("onShow: tag={}", tag);
 		}
+
 		@Override
-		public void onAdFailedToLoad(int errorCode) {
-			log.i("onAdFailedToLoad: errorCode=%d", errorCode);
+		public void onClick(String tag) {
+			log.i("onClick: tag={}", tag);
 		}
+
 		@Override
-		public void onAdLeftApplication() {
-			log.i("onAdLeftApplication");
+		public void onHide(String tag) {
+			log.i("onHide: tag={}", tag);
 		}
+
 		@Override
-		public void onAdLoaded() {
-			log.i("onAdLoaded");
+		public void onFailedToShow(String tag) {
+			log.i("onFailedToSHow: tag={}", tag);
 		}
+
 		@Override
-		public void onAdOpened() {
-			log.i("onAdOpened");
+		public void onAvailable(String tag) {
+			log.i("onAvailable: tag={}", tag);
 		}
-	}
+
+		@Override
+		public void onFailedToFetch(String tag) {
+			log.i("onFailedToFetch: tag={}", tag);
+		}
+
+		@Override
+		public void onAudioStarted() {
+			log.i("onAudioStarted");
+		}
+
+		@Override
+		public void onAudioFinished() {
+			log.i("onAudioFinished");
+		}
+	};
 
 	private static class AdHandler extends Handler {
 		@Override
@@ -52,25 +70,20 @@ public class AndroidAdController implements AdController {
 		}
 
 		void preload() {
-			log.i("preloadAd");
-			AdRequest.Builder builder = new AdRequest.Builder();
-			builder.tagForChildDirectedTreatment(true);
-			builder.addTestDevice(AdRequest.DEVICE_ID_EMULATOR);
-			builder.addTestDevice("A5A48A10EF5E078F1B701AF026C0298C"); // NEXUS-S
-			mInterstitial.loadAd(builder.build());
+			log.i("preload");
+			InterstitialAd.fetch();
 		}
 
 		void show() {
-			if (mInterstitial.isLoaded()) {
-				log.i("showAd: showing");
-				mInterstitial.show();
+			if (InterstitialAd.isAvailable()) {
+				log.i("show: ad is available");
+				InterstitialAd.display(mActivity);
 			} else {
-				log.e("showAd: not loaded");
+				log.i("show: ad is not available");
 			}
 		}
 
 		Activity mActivity;
-		InterstitialAd mInterstitial;
 		NLog log;
 	};
 
@@ -79,12 +92,12 @@ public class AndroidAdController implements AdController {
 
 	public AndroidAdController(Activity activity) {
 		log = NLog.createForClass(this);
+		HeyzapAds.start(activity);
+		InterstitialAd.fetch();
+		HeyzapAds.setOnStatusListener(new StatusListener());
 		mHandler = new AdHandler();
 		mHandler.mActivity = activity;
 		mHandler.log = log;
-		mHandler.mInterstitial = new InterstitialAd(mHandler.mActivity);
-		mHandler.mInterstitial.setAdListener(new LogAdListener());
-		mHandler.mInterstitial.setAdUnitId(INTERSTITIAL_ID);
 	}
 
 	@Override
