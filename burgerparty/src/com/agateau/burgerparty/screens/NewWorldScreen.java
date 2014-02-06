d1144092f21f79ffe4c1e5e00dae6c00d9fbfba4
Aurélien Gâteau <agateau@kde.org>
2014-02-07 00:12:37 +0100

    NewWorld: Improve animation, add delay at the end

--- burgerparty/src/com/agateau/burgerparty/screens/NewWorldScreen.java
+++ burgerparty/src/com/agateau/burgerparty/screens/NewWorldScreen.java
@@ -6,9 +6,11 @@ import com.agateau.burgerparty.utils.RefreshHelper;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Bezier;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.XmlReader;
 
@@ -16,16 +18,73 @@ public class NewWorldScreen extends BurgerPartyScreen {
 	private static final float ZOOM_DURATION_INTERVAL = 1.2f;
 	private int mWorldIndex;
 	private float mDuration;
-	private float mTime = 0;
 
 	private float mDotInterval;
-	private float mLastDotTime = 0;
 	private TextureRegion mDotRegion;
 
 	private Bezier<Vector2> mPath = new Bezier<Vector2>();
 	private Vector2 mTmpV = new Vector2();
 	private Image mPlane;
 	private Image mBackground;
+
+	private class FlyAction extends Action {
+		private float mTime = 0;
+		private float mLastDotTime = 0;
+
+		@Override
+		public boolean act(float delta) {
+			mTime += delta;
+			if (mTime > mDuration) {
+				mTime = mDuration;
+				return true;
+			}
+
+			float scale = computeScale();
+			mPlane.setScale(scale);
+
+			float oldX = mPlane.getX();
+			float oldY = mPlane.getY();
+			mPath.valueAt(mTmpV, mTime / mDuration);
+			mPlane.setPosition(mTmpV.x - mPlane.getWidth() / 2, mTmpV.y - mPlane.getHeight() / 2);
+
+			float angle = MathUtils.atan2(mPlane.getY() - oldY, mPlane.getX() - oldX);
+			mPlane.setRotation(MathUtils.radiansToDegrees * angle);
+
+			if (mTime - mLastDotTime > mDotInterval) {
+				addDot(scale / 2); // FIXME: draw smaller dots
+				mLastDotTime = mTime;
+			}
+			Camera camera = getStage().getCamera();
+			float xBorder = getStage().getWidth() / 2;
+			float yBorder = getStage().getHeight() / 2;
+			camera.position.x = MathUtils.clamp(MathUtils.round(mPlane.getX()), xBorder, mBackground.getWidth() - xBorder);
+			camera.position.y = MathUtils.clamp(MathUtils.round(mPlane.getY()), yBorder, mBackground.getHeight() - yBorder);
+			return false;
+		}
+
+		private float computeScale() {
+			float k = 1;
+			if (mTime < ZOOM_DURATION_INTERVAL) {
+				k = mTime / ZOOM_DURATION_INTERVAL;
+			} else if (mTime > mDuration - ZOOM_DURATION_INTERVAL) {
+				k = (mDuration - mTime) / ZOOM_DURATION_INTERVAL;
+			}
+			return Interpolation.pow2Out.apply(0.4f, 1, k);
+		}
+
+		private void addDot(float scale) {
+			Image image = new Image(mDotRegion);
+			mPath.valueAt(mTmpV, mTime / mDuration);
+			image.setPosition(
+					MathUtils.round(mTmpV.x - image.getWidth() / 2),
+					MathUtils.round(mTmpV.y - image.getHeight() / 2)
+					);
+			image.setOrigin(image.getWidth() / 2, image.getHeight() / 2);
+			image.setScale(scale);
+			getStage().addActor(image);
+			mPlane.toFront();
+		}
+	}
 
 	public NewWorldScreen(BurgerPartyGame game, int worldIndex) {
 		super(game);
@@ -36,68 +95,23 @@ public class NewWorldScreen extends BurgerPartyScreen {
 		createPlane();
 		loadXml();
 		createRefreshHelper();
+		getStage().getRoot().addAction(
+			Actions.sequence(
+				new FlyAction(),
+				Actions.delay(2),
+				Actions.run(new Runnable() {
+					@Override
+					public void run() {
+						startNextLevel();
+					}
+				})
+			)
+		);
 	}
 
 	@Override
 	public void onBackPressed() {
 		startNextLevel();
-	}
-
-	@Override
-	public void render(float delta) {
-		super.render(delta);
-		boolean done = act(delta);
-		if (done) {
-			startNextLevel();
-		}
-	}
-
-	private boolean act(float delta) {
-		mTime += delta;
-		if (mTime > mDuration) {
-			mTime = mDuration;
-			return true;
-		}
-
-		float scale = 1;
-		if (mTime < ZOOM_DURATION_INTERVAL) {
-			scale = mTime / ZOOM_DURATION_INTERVAL;
-		} else if (mTime > mDuration - ZOOM_DURATION_INTERVAL) {
-			scale = (mDuration - mTime) / ZOOM_DURATION_INTERVAL;
-		}
-		mPlane.setScale(1 - (float)Math.pow(scale - 1, 2));
-
-		float oldX = mPlane.getX();
-		float oldY = mPlane.getY();
-		mPath.valueAt(mTmpV, mTime / mDuration);
-		mPlane.setPosition(mTmpV.x - mPlane.getWidth() / 2, mTmpV.y - mPlane.getHeight() / 2);
-
-		float angle = MathUtils.atan2(mPlane.getY() - oldY, mPlane.getX() - oldX);
-		mPlane.setRotation(MathUtils.radiansToDegrees * angle);
-
-		if (mTime - mLastDotTime > mDotInterval) {
-			addDot(scale);
-			mLastDotTime = mTime;
-		}
-		Camera camera = getStage().getCamera();
-		float xBorder = getStage().getWidth() / 2;
-		float yBorder = getStage().getHeight() / 2;
-		camera.position.x = MathUtils.clamp(MathUtils.round(mPlane.getX()), xBorder, mBackground.getWidth() - xBorder);
-		camera.position.y = MathUtils.clamp(MathUtils.round(mPlane.getY()), yBorder, mBackground.getHeight() - yBorder);
-		return false;
-	}
-
-	private void addDot(float scale) {
-		Image image = new Image(mDotRegion);
-		mPath.valueAt(mTmpV, mTime / mDuration);
-		image.setPosition(
-				MathUtils.round(mTmpV.x - image.getWidth() / 2),
-				MathUtils.round(mTmpV.y - image.getHeight() / 2)
-				);
-		image.setOrigin(image.getWidth() / 2, image.getHeight() / 2);
-		image.setScale(scale);
-		getStage().addActor(image);
-		mPlane.toFront();
 	}
 
 	private void createBackground() {
