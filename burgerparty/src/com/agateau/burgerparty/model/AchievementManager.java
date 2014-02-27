f30abe426536f2d4fc9e097d60c0b86245dc8541
Aurélien Gâteau <agateau@kde.org>
2014-02-27 09:58:46 +0100

    Move GameStat serializing code to a GameStatManager class

--- burgerparty/src/com/agateau/burgerparty/model/AchievementManager.java
+++ burgerparty/src/com/agateau/burgerparty/model/AchievementManager.java
@@ -17,91 +17,36 @@ public class AchievementManager {
 
 	public Signal1<Achievement> achievementUnlocked = new Signal1<Achievement>();
 	
-	private HashMap<String, GameStat> mGameStats = new HashMap<String, GameStat>();
 	private HashMap<String, Achievement> mAchievements = new HashMap<String, Achievement>();
-	private FileHandle mGameStatsFileHandle;
-	private FileHandle mAchievementsFileHandle;
+	private FileHandle mFileHandle;
 
 	private NLog log = NLog.getRoot().create("AchievementManager");
 
-	public void addGameStat(GameStat gameStat) {
-		mGameStats.put(gameStat.getId(), gameStat);
-		gameStat.changed.connect(mHandlers, new Signal0.Handler() {
-			@Override
-			public void handle() {
-				scheduleGameStatsSave();
-			}
-		});
-	}
-
-	public void addAchievement(final Achievement achievement) {
+	public void add(final Achievement achievement) {
 		assert(achievement != null);
 		mAchievements.put(achievement.getId(), achievement);
 		achievement.unlocked.connect(mHandlers, new Signal0.Handler() {
 			@Override
 			public void handle() {
-				scheduleAchievementsSave();
+				scheduleSave();
 				achievementUnlocked.emit(achievement);
 			}
 		});
 	}
 
-	public void onLevelStarted(World world) {
-		for(GameStat stat: mGameStats.values()) {
-			stat.onLevelStarted(world);
-		}
-	}
-
-	/// Gamestats load/save
-	public void setGameStatsFileHandle(FileHandle handle) {
-		mGameStatsFileHandle = handle;
-	}
-
-	public void loadGameStats(XmlReader.Element root) {
-		for(int idx = 0; idx < root.getChildCount(); ++idx) {
-			XmlReader.Element element = root.getChild(idx);
-			String id = element.getAttribute("id");
-			GameStat stat = mGameStats.get(id);
-			if (stat == null) {
-				log.e("No gamestat with id '%s'", id);
-				continue;
-			}
-			stat.load(element);
-		}
-	}
-
-	public void saveGameStats() {
-		XmlWriter writer = new XmlWriter(mGameStatsFileHandle.writer(false));
-		saveGameStats(writer);
-	}
-
-	public void saveGameStats(XmlWriter writer) {
-		try {
-			XmlWriter root = writer.element("gamestats");
-			for (GameStat stat: mGameStats.values()) {
-				XmlWriter element = root.element("gamestat");
-				element.attribute("id", stat.getId());
-				stat.save(element);
-				element.pop();
-			}
-			writer.close();
-		} catch (IOException e) {
-			log.e("saveGameStats: Failed to save gamestats. Exception: %s", e);
-		}
-	}
-
-	private void scheduleGameStatsSave() {
-		// FIXME: Really schedule
-		saveGameStats();
-	}
-	/// /Gamestats load/save
-
 	/// Achievements load/save
-	public void setAchievementsFileHandle(FileHandle handle) {
-		mAchievementsFileHandle = handle;
+	public void setFileHandle(FileHandle handle) {
+		mFileHandle = handle;
 	}
 
-	public void loadAchievements(XmlReader.Element root) {
+	public void load() {
+		assert(mFileHandle != null);
+		if (mFileHandle.exists()) {
+			load(FileUtils.parseXml(mFileHandle));
+		}
+	}
+
+	public void load(XmlReader.Element root) {
 		for(int idx = 0; idx < root.getChildCount(); ++idx) {
 			XmlReader.Element element = root.getChild(idx);
 			String id = element.getAttribute("id");
@@ -114,12 +59,12 @@ public class AchievementManager {
 		}
 	}
 
-	public void saveAchievements() {
-		XmlWriter writer = new XmlWriter(mAchievementsFileHandle.writer(false));
-		saveAchievements(writer);
+	public void save() {
+		XmlWriter writer = new XmlWriter(mFileHandle.writer(false));
+		save(writer);
 	}
 
-	public void saveAchievements(XmlWriter writer) {
+	public void save(XmlWriter writer) {
 		try {
 			XmlWriter root = writer.element("achievements");
 			for (Achievement achievement: mAchievements.values()) {
@@ -136,20 +81,8 @@ public class AchievementManager {
 		}
 	}
 
-	private void scheduleAchievementsSave() {
+	private void scheduleSave() {
 		// FIXME: Really schedule
-		saveAchievements();
-	}
-	/// /Achievements load/save
-
-	public void load() {
-		assert(mGameStatsFileHandle != null);
-		assert(mAchievementsFileHandle != null);
-		if (mGameStatsFileHandle.exists()) {
-			loadGameStats(FileUtils.parseXml(mGameStatsFileHandle));
-		}
-		if (mAchievementsFileHandle.exists()) {
-			loadAchievements(FileUtils.parseXml(mAchievementsFileHandle));
-		}
+		save();
 	}
 }
--- burgerparty/src/com/agateau/burgerparty/model/BurgerPartyGameStats.java
+++ burgerparty/src/com/agateau/burgerparty/model/BurgerPartyGameStats.java
@@ -1,28 +1,25 @@
 package com.agateau.burgerparty.model;
 
-import java.util.HashSet;
-
-import com.agateau.burgerparty.model.World.Score;
 import com.agateau.burgerparty.utils.FileUtils;
-import com.agateau.burgerparty.utils.Signal1;
 
 public class BurgerPartyGameStats {
-	private HashSet<Object> mHandlers = new HashSet<Object>();
-
 	public final CounterGameStat mealServedCount;
 
 	public final AchievementManager manager = new AchievementManager();
 
+	private final GameStatManager mGameStatManager = new GameStatManager();
+
 	public BurgerPartyGameStats() {
 		mealServedCount = new CounterGameStat("mealServedCount");
-		manager.addGameStat(mealServedCount);
+		mGameStatManager.add(mealServedCount);
+		mGameStatManager.setFileHandle(FileUtils.getUserWritableFile("gamestats.xml"));
+		mGameStatManager.load();
 
 		CounterAchievement achievement = new CounterAchievement("burger-master", "Burger Master", "Serve 10 burgers");
 		achievement.init(mealServedCount, 10);
-		manager.addAchievement(achievement);
+		manager.add(achievement);
 
-		manager.setGameStatsFileHandle(FileUtils.getUserWritableFile("gamestats.xml"));
-		manager.setAchievementsFileHandle(FileUtils.getUserWritableFile("achievements.xml"));
+		manager.setFileHandle(FileUtils.getUserWritableFile("achievements.xml"));
 		manager.load();
 	}
 }
