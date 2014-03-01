82be57c94a3fb2198ff5185b7632d43b38415876
Aurélien Gâteau <agateau@kde.org>
2014-03-01 14:40:15 +0100

    Use different achievement icons

--- burgerparty/src/com/agateau/burgerparty/view/AchievementView.java
+++ burgerparty/src/com/agateau/burgerparty/view/AchievementView.java
@@ -4,6 +4,8 @@ import com.agateau.burgerparty.Assets;
 import com.agateau.burgerparty.utils.Achievement;
 import com.agateau.burgerparty.utils.Anchor;
 import com.agateau.burgerparty.utils.AnchorGroup;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
@@ -12,22 +14,26 @@ public class AchievementView extends AnchorGroup {
 
 	public AchievementView(Assets assets, Achievement achievement) {
 		setSpacing(4);
-		Image bg = new Image(assets.getTextureAtlas().createPatch("ui/achievement-bg"));
+		TextureAtlas atlas = assets.getTextureAtlas();
+		Image bg = new Image(atlas.createPatch("ui/achievement-bg"));
 		bg.setWidth(WIDTH);
 		addActor(bg);
 		setSize(bg.getWidth(), bg.getHeight());
 
-		// FIXME: Get icon name from Achievement class
-		Image icon = new Image(assets.getTextureAtlas().findRegion("ui/achievements/burger-master"));
+		TextureRegion region = atlas.findRegion("achievements/" + achievement.getId());
+		if (region == null) {
+			region = atlas.findRegion("achievements/generic");
+		}
+		Image icon = new Image(region);
 		Label titleLabel = new Label(achievement.getTitle(), assets.getSkin(), "achievement-title");
 		Label descriptionLabel = new Label(achievement.getDescription(), assets.getSkin(), "achievement-description");
 
-		addRule(icon, Anchor.CENTER_LEFT, this, Anchor.CENTER_LEFT, 1, 0);
+		addRule(icon, Anchor.BOTTOM_LEFT, this, Anchor.BOTTOM_LEFT, 1, 1);
 		addRule(titleLabel, Anchor.BOTTOM_LEFT, icon, Anchor.CENTER_RIGHT, 1, -2.5f);
 		addRule(descriptionLabel, Anchor.TOP_LEFT, icon, Anchor.CENTER_RIGHT, 1, 0.5f);
 
 		if (achievement.isUnlocked()) {
-			Image unlocked = new Image(assets.getTextureAtlas().findRegion("ui/achievement-unlocked"));
+			Image unlocked = new Image(atlas.findRegion("ui/achievement-unlocked"));
 			addRule(unlocked, Anchor.CENTER_RIGHT, this, Anchor.CENTER_RIGHT, -1, 0);
 		}
 	}
