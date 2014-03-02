1ab395af725d8f13171b2b93526812dc619ca5ff
Aurélien Gâteau <agateau@kde.org>
2014-03-02 22:47:50 +0100

    Experiment with new achievement appearance

--- burgerparty/src/com/agateau/burgerparty/view/AchievementView.java
+++ burgerparty/src/com/agateau/burgerparty/view/AchievementView.java
@@ -13,29 +13,29 @@ public class AchievementView extends AnchorGroup {
 	private static final float WIDTH = 600;
 
 	public AchievementView(Assets assets, Achievement achievement) {
-		setSpacing(4);
 		TextureAtlas atlas = assets.getTextureAtlas();
 		Image bg = new Image(atlas.createPatch("ui/achievement-bg"));
 		bg.setWidth(WIDTH);
 		addActor(bg);
 		setSize(bg.getWidth(), bg.getHeight());
 
-		TextureRegion region = atlas.findRegion("achievements/" + achievement.getId());
-		if (region == null) {
-			region = atlas.findRegion("achievements/generic");
+		TextureRegion iconRegion = atlas.findRegion("achievements/" + achievement.getId());
+		if (iconRegion == null) {
+			iconRegion = atlas.findRegion("achievements/generic");
 		}
-		Image icon = new Image(region);
+
+		TextureRegion statusRegion = atlas.findRegion("ui/achievement-" + (achievement.isUnlocked() ? "unlocked" : "locked"));
+
+		Image icon = new Image(iconRegion);
 		Label titleLabel = new Label(achievement.getTitle(), assets.getSkin(), "achievement-title");
 		Label descriptionLabel = new Label(achievement.getDescription(), assets.getSkin(), "achievement-description");
 
-		addRule(icon, Anchor.BOTTOM_LEFT, this, Anchor.BOTTOM_LEFT, 1, 1);
-		addRule(titleLabel, Anchor.BOTTOM_LEFT, icon, Anchor.CENTER_RIGHT, 1, -2.5f);
-		addRule(descriptionLabel, Anchor.TOP_LEFT, icon, Anchor.CENTER_RIGHT, 1, 0.5f);
+		Image status = new Image(statusRegion);
 
-		if (achievement.isUnlocked()) {
-			Image unlocked = new Image(atlas.findRegion("ui/achievement-unlocked"));
-			addRule(unlocked, Anchor.CENTER_RIGHT, this, Anchor.CENTER_RIGHT, -1, 0);
-		}
+		addRule(icon, Anchor.CENTER_LEFT, this, Anchor.CENTER_LEFT, 6, 0);
+		addRule(titleLabel, Anchor.BOTTOM_LEFT, icon, Anchor.CENTER_RIGHT, 4, -10f);
+		addRule(descriptionLabel, Anchor.TOP_LEFT, icon, Anchor.CENTER_RIGHT, 4, 2f);
+		addRule(status, Anchor.CENTER_RIGHT, this, Anchor.CENTER_RIGHT, -12, 0);
 	}
 
 }
