610bfb82930dd143f2b17406dbaff3ba2b09cd3b
Aurélien Gâteau <agateau@kde.org>
2014-03-05 23:47:10 +0100

    Use a smaller view when unlocking achievements

--- burgerparty/src/com/agateau/burgerparty/view/AchievementViewController.java
+++ burgerparty/src/com/agateau/burgerparty/view/AchievementViewController.java
@@ -13,14 +13,14 @@ public class AchievementViewController {
 	private static final float STAY_DURATION = 2;
 
 	private BurgerPartyGame mGame;
-	private Array<AchievementView> mViews = new Array<AchievementView>();
+	private Array<AchievementSmallView> mViews = new Array<AchievementSmallView>();
 
 	public AchievementViewController(BurgerPartyGame game) {
 		mGame = game;
 	}
 
 	public void show(Achievement achievement) {
-		AchievementView view = new AchievementView(mGame.getAssets(), achievement);
+		AchievementSmallView view = new AchievementSmallView(mGame.getAssets(), achievement);
 		mViews.add(view);
 		if (mViews.size == 1) {
 			showNextView();
@@ -34,15 +34,16 @@ public class AchievementViewController {
 		StageScreen screen = (StageScreen)mGame.getScreen();
 		assert(screen != null);
 		Stage stage = screen.getStage();
-		AchievementView view = mViews.removeIndex(0);
+		final AchievementSmallView view = mViews.removeIndex(0);
 
 		screen.addNotificationActor(view);
 		view.setPosition((stage.getWidth() - view.getWidth()) / 2, stage.getHeight());
+		float viewHeight = view.getHeight();// - AchievementSmallView.PADDING;
 		view.addAction(
 			Actions.sequence(
-				Actions.moveBy(0, -view.getHeight(), SLIDE_DURATION, Interpolation.pow2Out),
+				Actions.moveBy(0, -viewHeight, SLIDE_DURATION, Interpolation.pow2Out),
 				Actions.delay(STAY_DURATION),
-				Actions.moveBy(0, view.getHeight(), SLIDE_DURATION, Interpolation.pow2In),
+				Actions.moveBy(0, viewHeight, SLIDE_DURATION, Interpolation.pow2In),
 				Actions.run(new Runnable() {
 					@Override
 					public void run() {
