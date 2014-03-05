223b4734bb088a19a20c0b50fc8117dd90361ae4
Aurélien Gâteau <agateau@kde.org>
2014-03-05 15:10:13 +0100

    Add morning and evening gamer achievements

--- burgerparty/src/com/agateau/burgerparty/model/BurgerPartyGameStats.java
+++ burgerparty/src/com/agateau/burgerparty/model/BurgerPartyGameStats.java
@@ -1,5 +1,8 @@
 package com.agateau.burgerparty.model;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashSet;
 
 import com.agateau.burgerparty.utils.Achievement;
@@ -9,23 +12,30 @@ import com.agateau.burgerparty.utils.CounterGameStat;
 import com.agateau.burgerparty.utils.FileUtils;
 import com.agateau.burgerparty.utils.GameStatManager;
 import com.agateau.burgerparty.utils.Signal0;
+import com.agateau.burgerparty.utils.StringListGameStat;
 
 import static com.agateau.burgerparty.utils.I18n._;
 
 public class BurgerPartyGameStats {
 	private HashSet<Object> mHandlers = new HashSet<Object>();
 
-	public final CounterGameStat mealServedCount;
+	public final CounterGameStat mealServedCount = new CounterGameStat("mealServedCount");
+	public final StringListGameStat morningPlayDates = new StringListGameStat("morningPlayDates");
+	public final StringListGameStat eveningPlayDates = new StringListGameStat("eveningPlayDates");
 
 	public final AchievementManager manager = new AchievementManager();
 
 	private final GameStatManager mGameStatManager = new GameStatManager();
 
 	private Achievement mCloseCall;
+	private Achievement mMorningGamer;
+	private Achievement mEveningGamer;
 
 	public BurgerPartyGameStats() {
-		mealServedCount = new CounterGameStat("mealServedCount");
+		mGameStatManager.add(morningPlayDates);
+		mGameStatManager.add(eveningPlayDates);
 		mGameStatManager.add(mealServedCount);
+
 		mGameStatManager.setFileHandle(FileUtils.getUserWritableFile("gamestats.xml"));
 		mGameStatManager.load();
 
@@ -40,6 +50,12 @@ public class BurgerPartyGameStats {
 		mCloseCall = new Achievement("close-call", _("Close Call"), _("Finish a level with 3 seconds left."));
 		manager.add(mCloseCall);
 
+		mMorningGamer = new Achievement("morning-gamer", _("Morning Gamer"), _("Start a game between 7AM and 10AM for 4 days"));
+		manager.add(mMorningGamer);
+
+		mEveningGamer = new Achievement("evening-gamer", _("Evening Gamer"), _("Start a game between 7PM and 11PM for 4 days"));
+		manager.add(mEveningGamer);
+
 		manager.setFileHandle(FileUtils.getUserWritableFile("achievements.xml"));
 		manager.load();
 	}
@@ -52,5 +68,33 @@ public class BurgerPartyGameStats {
 				}
 			}
 		});
+
+		updateDateGameStats();
+	}
+
+	static private final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+	private void updateDateGameStats() {
+		GregorianCalendar calendar = new GregorianCalendar();
+		int hour = calendar.get(Calendar.HOUR_OF_DAY);
+		String dateString = DATE_FORMAT.format(calendar.getTime());
+
+		updateDateGameStat(hour, dateString, morningPlayDates, mMorningGamer, 7, 10);
+		updateDateGameStat(hour, dateString, eveningPlayDates, mEveningGamer, 19, 23);
+	}
+
+	private void updateDateGameStat(int hour, String dateString, StringListGameStat stat, Achievement achievement, int minHour, int maxHour) {
+		if (achievement.isUnlocked()) {
+			return;
+		}
+		if (hour < minHour || hour >= maxHour) {
+			return;
+		}
+		if (stat.contains(dateString)) {
+			return;
+		}
+		stat.add(dateString);
+		if (stat.getCount() >= 4) {
+			achievement.unlock();
+		}
 	}
 }
--- burgerparty/src/com/agateau/burgerparty/utils/GameStatManager.java
+++ burgerparty/src/com/agateau/burgerparty/utils/GameStatManager.java
@@ -37,9 +37,11 @@ public class GameStatManager {
 	}
 
 	public void load(XmlReader.Element root) {
+		NLog.getRoot().i("GameStatManager.load");
 		for(int idx = 0; idx < root.getChildCount(); ++idx) {
 			XmlReader.Element element = root.getChild(idx);
 			String id = element.getAttribute("id");
+			NLog.getRoot().i("GameStatManager.load id=%s", id);
 			GameStat stat = mGameStats.get(id);
 			if (stat == null) {
 				log.e("No gamestat with id '%s'", id);
