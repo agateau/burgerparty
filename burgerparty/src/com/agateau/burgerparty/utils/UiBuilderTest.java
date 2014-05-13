e027d4ac9fd3795cc254ef986425021ca9709a0f
Aurélien Gâteau <agateau@kde.org>
2014-05-13 23:54:56 +0200

    UiBuilder: Reset mActorForId to be able to reuse builder

---
+++ burgerparty/src/com/agateau/burgerparty/utils/UiBuilderTest.java
@@ -0,0 +1,37 @@
+package com.agateau.burgerparty.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.agateau.burgerparty.model.TestUtils;
+import com.badlogic.gdx.utils.XmlReader;
+
+public class UiBuilderTest {
+    @Test
+    public void testSimple() {
+        UiBuilder builder = new UiBuilder(null, null);
+        XmlReader.Element rootElement = TestUtils.parseXml(
+            "<gdxui><AnchorGroup id='root'></AnchorGroup></gdxui>"
+        );
+        builder.build(rootElement);
+        AnchorGroup group = builder.getActor("root");
+        assertNotNull(group);
+    }
+
+    @Test
+    public void testInsideReference() {
+        UiBuilder builder = new UiBuilder(null, null);
+        XmlReader.Element rootElement = TestUtils.parseXml(
+              "<gdxui>"
+            + "  <AnchorGroup id='root'>"
+            + "    <ScrollPane bottomLeft='root.bottomLeft 1 1'>"
+            + "    </ScrollPane>"
+            + "  </AnchorGroup>"
+            + "</gdxui>"
+        );
+        builder.build(rootElement);
+        AnchorGroup group = builder.getActor("root");
+        assertNotNull(group);
+    }
+}
