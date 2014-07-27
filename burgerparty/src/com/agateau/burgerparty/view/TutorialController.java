4acc90f900c4af5b16df822074c4b70d0a0e685e
Aurélien Gâteau <agateau@kde.org>
2014-07-27 23:40:32 +0200

    Add pulsing circle under tutorial finger

--- burgerparty/src/com/agateau/burgerparty/view/TutorialController.java
+++ burgerparty/src/com/agateau/burgerparty/view/TutorialController.java
@@ -11,31 +11,46 @@ import com.agateau.burgerparty.utils.Signal0;
 import com.agateau.burgerparty.utils.Signal1;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class TutorialController {
     private ConnectionManager mMealConnections = new ConnectionManager();
     private World mWorld;
     private InventoryView mInventoryView;
+    private Group mGroup;
     private Image mIndicator;
-    private MoveToAction mMoveAction = null;
+    private Image mPulsingCircle;
+    private Action mMoveAction = null;
 
-    private static final float MOVE_DURATION = 0.5f; 
+    private static final float MOVE_DURATION = 0.5f;
+
+    private static final float PULSING_SCALE_MIN = 0.8f;
+    private static final float PULSING_DURATION = 0.6f;
 
     public TutorialController(BurgerPartyGame game, World world, InventoryView inventoryView) {
-        mIndicator = new Image(game.getAssets().getTextureAtlas().findRegion("ui/finger"));
-        mIndicator.setTouchable(Touchable.disabled);
         mWorld = world;
         mInventoryView = inventoryView;
+
+        mGroup = new Group();
+        mGroup.setTouchable(Touchable.disabled);
+        mIndicator = new Image(game.getAssets().getTextureAtlas().findRegion("ui/finger"));
+
+        mPulsingCircle = new Image(game.getAssets().getTextureAtlas().findRegion("ui/finger-circle"));
+        mPulsingCircle.setOrigin(mPulsingCircle.getWidth() / 2, mPulsingCircle.getHeight() / 2);
+
+        mGroup.addActor(mPulsingCircle);
+        mGroup.addActor(mIndicator);
+        mPulsingCircle.setPosition(-13, mIndicator.getHeight() - 28);
         updateConnections();
     }
 
     public Actor getIndicator() {
-        return mIndicator;
+        return mGroup;
     }
 
     public void onNewMeal() {
@@ -74,21 +89,26 @@ public class TutorialController {
     private void updateIndicator() {
         MealItem nextItem = findNextItem();
         if (nextItem == null) {
-            mIndicator.setVisible(false);
+            mGroup.setVisible(false);
             return;
         }
 
-        mIndicator.setVisible(true);
+        mGroup.setVisible(true);
         Vector2 pos = new Vector2();
         mInventoryView.getItemPosition(nextItem, pos);
         mInventoryView.localToAscendantCoordinates(mIndicator.getParent(), pos);
         pos.y -= mIndicator.getHeight();
 
         if (mMoveAction != null) {
-            mIndicator.removeAction(mMoveAction);
+            mGroup.removeAction(mMoveAction);
         }
-        mMoveAction = Actions.moveTo(pos.x, pos.y, MOVE_DURATION, Interpolation.pow2);
-        mIndicator.addAction(mMoveAction);
+        mPulsingCircle.setColor(1, 1, 1, 0);
+        mPulsingCircle.setScale(1);
+        mMoveAction = Actions.sequence(
+            Actions.moveTo(pos.x, pos.y, MOVE_DURATION, Interpolation.pow2),
+            Actions.addAction(createPulseAction(), mPulsingCircle)
+        );
+        mGroup.addAction(mMoveAction);
     }
 
     private MealItem findNextItem() {
@@ -107,5 +127,13 @@ public class TutorialController {
         }
 
         return null;
+    }
+
+    private static Action createPulseAction() {
+        return Actions.parallel(
+            Actions.alpha(1),
+            Actions.scaleTo(PULSING_SCALE_MIN, PULSING_SCALE_MIN, PULSING_DURATION, Interpolation.pow2),
+            Actions.alpha(0, PULSING_DURATION)
+        );
     }
 }
