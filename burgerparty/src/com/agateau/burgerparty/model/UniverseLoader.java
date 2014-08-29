2ca90f76a558a2964d1b44876f519e5dc906d9d3
Aurélien Gâteau <agateau@kde.org>
2014-08-29 23:45:43 +0200

    Even a bit more easier

--- burgerparty/src/com/agateau/burgerparty/model/UniverseLoader.java
+++ burgerparty/src/com/agateau/burgerparty/model/UniverseLoader.java
@@ -16,7 +16,7 @@ public class UniverseLoader {
     private static final int TIME_STEP = 5;
     private static final boolean DEBUG_DURATION = false;
     private static final float MIN_EASINESS = 1;
-    private static final float MAX_EASINESS = 2.2f;
+    private static final float MAX_EASINESS = 2.4f;
 
     private FileHandle mCsvHandle;
     private Writer mCsvWriter;
