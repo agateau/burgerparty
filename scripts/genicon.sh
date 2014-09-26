6a3d8993cbfe20a4439a431691ad0063c27e2a6b
Aurélien Gâteau <mail@agateau.com>
2014-09-26 10:47:10 +0200

    Nicer icon

--- scripts/genicon.sh
+++ scripts/genicon.sh
@@ -1,9 +1,14 @@
-cd $(dirname $0)
+#!/bin/sh -e
 
+cd $(dirname $0)/..
+
+EXTRA_DIR=extra
 RES_DIR=burgerparty-android/res
 
 NAME=ic_launcher.png
 SRC=$RES_DIR/drawable-hdpi/$NAME
+
+convert $EXTRA_DIR/ic_launcher-raw.png $EXTRA_DIR/mask.png -compose DstOut -composite $SRC
 
 convert -resize 48x48 $SRC $RES_DIR/drawable-mdpi/$NAME
 convert -resize 36x36 $SRC $RES_DIR/drawable-ldpi/$NAME
