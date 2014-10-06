db2ad3c116450e839192d5367c1aa9c3fce0207a
Aurélien Gâteau <mail@agateau.com>
2014-10-06 23:36:14 +0200

    Bump version numbers

--- burgerparty/src/com/agateau/burgerparty/Constants.java
+++ burgerparty/src/com/agateau/burgerparty/Constants.java
@@ -1,7 +1,7 @@
 package com.agateau.burgerparty;
 
 public class Constants {
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1.0";
 
     public static final int LEVEL_PER_WORLD = 15;
     public static final int WORLD_COUNT = 3;
