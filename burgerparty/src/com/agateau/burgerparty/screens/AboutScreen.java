bdd7dbb31d9e1843d51cfc24c1e3bba23f0a9d0b
Aurélien Gâteau <mail@agateau.com>
2014-10-12 23:29:05 +0200

    tr--

--- burgerparty/src/com/agateau/burgerparty/screens/AboutScreen.java
+++ burgerparty/src/com/agateau/burgerparty/screens/AboutScreen.java
@@ -89,7 +89,7 @@ public class AboutScreen extends BurgerPartyScreen {
 
         addHeading(tr("Sounds"));
         addText("Clara Gâteau");
-        addText(tr("Thomas Tripon"));
+        addText("Thomas Tripon");
         addText("AlienXXX - www.freesound.org/people/AlienXXX");
         addText("artisticdude - opengameart.org/content/inventory-sound-effects");
         addText("Autistic Lucario - www.freesound.org/people/Autistic%20Lucario");
