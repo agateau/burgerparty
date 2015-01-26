0d722c62354c74b2516024faa05c80a428e3c482
Aurélien Gâteau <mail@agateau.com>
2015-01-26 23:22:29 +0100

    Nicer config buttons

--- burgerparty/src/com/agateau/burgerparty/view/ConfigDialog.java
+++ burgerparty/src/com/agateau/burgerparty/view/ConfigDialog.java
@@ -7,12 +7,16 @@ import com.agateau.burgerparty.utils.MusicController;
 import com.agateau.burgerparty.utils.StageScreen;
 import com.agateau.burgerparty.utils.UiUtils;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
@@ -28,49 +32,72 @@ public class ConfigDialog extends Dialog {
     private final ConfigButton mMuteButton;
 
     private static class ConfigButton extends HorizontalGroup {
+        private final Label mSubtitleLabel;
         private ImageButton mButton;
 
-        public ConfigButton(Assets assets, String imageName, String text) {
-            this(assets, imageName, text, "default");
+        public ConfigButton(Assets assets, String imageName, String titleText, String subtitleText) {
+            this(assets, imageName, titleText, subtitleText, "default");
         }
 
-        public ConfigButton(Assets assets, String imageName, String text, String styleName) {
-            Label label = new Label(text, assets.getSkin(), "config-button");
-            mButton = BurgerPartyUiBuilder.createRoundButton(assets, imageName, styleName);
+        public ConfigButton(Assets assets, String imageName, String titleText, String subtitleText, String styleName) {
             setSpacing(UiUtils.SPACING / 2);
+
+            mButton = BurgerPartyUiBuilder.createRoundButton(assets, imageName, styleName);
+            mButton.setTouchable(Touchable.disabled);
+            setTouchable(Touchable.enabled);
+            addListener(mButton.getClickListener());
+
+            Label titleLabel = new Label(titleText, assets.getSkin(), "config-button-title");
+            mSubtitleLabel = new Label(subtitleText, assets.getSkin(), "config-button-subtitle");
+
             addActor(mButton);
-            addActor(label);
+            VerticalGroup vGroup = new VerticalGroup();
+            vGroup.setAlignment(Align.left);
+            vGroup.addActor(titleLabel);
+            vGroup.addActor(mSubtitleLabel);
+            vGroup.setSpacing(-10);
+
+            addActor(vGroup);
             setSize(getPrefWidth(), getPrefHeight());
         }
 
         public Image getImage() {
             return mButton.getImage();
         }
+
+        public void setSubtitleLabel(String subtitleLabel) {
+            mSubtitleLabel.setText(subtitleLabel);
+        }
     }
 
     public ConfigDialog(StageScreen stageScreen, final BurgerPartyGame game) {
         super(stageScreen, game.getAssets());
+        final float columnWidth = Gdx.app.getGraphics().getWidth() * 0.35f;
         mGame = game;
         Assets assets = game.getAssets();
-        mMuteButton = new ConfigButton(assets, "ui/icon-sound-on", tr("Sound"));
-        ConfigButton aboutButton = new ConfigButton(assets, "ui/icon-info", tr("About"));
-        ConfigButton facebookButton = new ConfigButton(assets, "ui/icon-fb", tr("Like us"), "fb-button");
-        ConfigButton gplusButton = new ConfigButton(assets, "ui/icon-gplus", tr("Add us"), "gplus-button");
-        ConfigButton rateButton = new ConfigButton(assets, "ui/icon-rate", tr("Rate Burger Party"));
+        mMuteButton = new ConfigButton(assets, "ui/icon-sound-on", tr("Sound"), "");
+        ConfigButton aboutButton = new ConfigButton(assets, "ui/icon-info", tr("About"), tr("Who made this?"));
+        ConfigButton facebookButton = new ConfigButton(assets, "ui/icon-fb", tr("Facebook"), tr("Become a fan"), "fb-button");
+        ConfigButton gplusButton = new ConfigButton(assets, "ui/icon-gplus", tr("Google+"), tr("Add us to your circles"), "gplus-button");
+        ConfigButton rateButton = new ConfigButton(assets, "ui/icon-rate", tr("Rate Burger Party"), tr("Like the game? Would be awesome if you could give it a good rate!"));
+
+        //mMuteButton.setWidth(columnWidth);
+        //aboutButton.setWidth(columnWidth);
+        //facebookButton.setWidth(columnWidth);
+        //gplusButton.setWidth(columnWidth);
 
         Table root = new Table(assets.getSkin());
         final float spacing = UiUtils.SPACING;
-        final float colWidth = stageScreen.getStage().getWidth() * 0.3f;
-        root.defaults().left().padBottom(spacing).padRight(spacing);
+        root.defaults().width(columnWidth).left().padBottom(spacing).padRight(spacing);
         root.padTop(spacing).padLeft(spacing);
-        root.add(mMuteButton).width(colWidth).uniform();
-        root.add(facebookButton).uniform();
+        root.add(mMuteButton);
+        root.add(facebookButton);
         root.row();
         root.add(aboutButton);
         root.add(gplusButton);
         root.row();
         root.add(rateButton).colspan(2);
-        root.setSize(root.getPrefWidth(), root.getPrefHeight());
+        root.setSize(MathUtils.round(root.getPrefWidth()), MathUtils.round(root.getPrefHeight()));
         setChild(root);
 
         mMuteButton.addListener(new ChangeListener() {
@@ -114,5 +141,6 @@ public class ConfigDialog extends Dialog {
         boolean muted = mGame.getMusicController().isMuted();
         Drawable drawable = mGame.getAssets().getSkin().getDrawable(muted ? "ui/icon-sound-off" : "ui/icon-sound-on");
         mMuteButton.getImage().setDrawable(drawable);
+        mMuteButton.setSubtitleLabel(muted ? tr("Sound is OFF") : tr("Sound is ON"));
     }
 }
