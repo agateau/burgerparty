05d43a61eb8518164cd6a74747858fde829d3827
Aurelien Gateau <mail@agateau.com>
2023-07-07 19:57:36 +0200

    Wrap achievement description to fix overflowing

--- core/i18n/Messages_eu.java
+++ core/i18n/Messages_eu.java
@@ -12,20 +12,17 @@ public class Messages_eu extends com.greenyetilab.linguaj.Messages {
         plainEntries.put("Buy Burger Party goodies!", "Erosi Burger Party-ko\ngutiziak!");
         plainEntries.put("Close Call", "Azken Deia");
         plainEntries.put("Code & Design", "Kodea & Diseinua");
-        plainEntries.put("Congratulations, you finished level %d-%d!",
-                "Zorionak, %d-%d maila bukatu duzu!");
+        plainEntries.put("Congratulations, you finished level %d-%d!", "Zorionak, %d-%d maila bukatu duzu!");
         plainEntries.put("Congratulations, you finished the game!", "Zorionak, jokoa bukatu duzu!");
         plainEntries.put("Congratulations, you finished world %d!", "Zorionak, %d mundua bukatu duzu!");
         plainEntries.put("Creative", "Sortzailea");
-        plainEntries.put("Evening Gamer", "Arratsaldeko jokalaria");
+        plainEntries.put("Evening Gamer", "Arratsaldeko Jokalaria");
         plainEntries.put("Fan", "Fan");
-        plainEntries.put("Finish all levels of world %d with 3 stars.",
-                "Bukatu %d munduko maila guztiak 3 izarrekin.");
+        plainEntries.put("Finish all levels of world %d with 3 stars.", "Bukatu %d munduko maila guztiak 3 izarrekin.");
         plainEntries.put("Follow me on Mastodon", "Jarrai nazazu Mastodon-en");
         plainEntries.put("Fonts", "Letra-tipoak");
         plainEntries.put("Game Over", "Akabo Jokoa");
-        plainEntries.put("Get a perfect in all levels of world %d.",
-                "Egizu bikain %d munduko maila guztietan.");
+        plainEntries.put("Get a perfect in all levels of world %d.", "Egizu bikain %d munduko maila guztietan.");
         plainEntries.put("Goodies", "Gutiziak");
         plainEntries.put("Happy customer!", "Bezeroa pozik!");
         plainEntries.put("High score: %d", "Marka: %d");
@@ -39,7 +36,7 @@ public class Messages_eu extends com.greenyetilab.linguaj.Messages {
         plainEntries.put("New item unlocked!", "Item berria lortu duzu!");
         plainEntries.put("No high score yet", "Oraindik markarik ez");
         plainEntries.put("Paused", "Pausatuta");
-        plainEntries.put("Perfect #%d", "#%d Bikain");
+        plainEntries.put("Perfect #%d", "#%d bikain");
         plainEntries.put("Practice Area", "Praktika Txokoa");
         plainEntries.put("Rate Burger Party", "Puntuatu Burger Party");
         plainEntries.put("Sound", "Soinua");
@@ -51,72 +48,82 @@ public class Messages_eu extends com.greenyetilab.linguaj.Messages {
         plainEntries.put("Testers", "Tester-ak");
         plainEntries.put("Thank you for playing!", "Eskerrik asko jostatzeagatik!");
         plainEntries.put("Thomas Tripon", "Thomas Tripon");
-        plainEntries.put("Version %s", "%s tertsioa");
+        plainEntries.put("Version %s", "%s bertsioa");
         plainEntries.put("Who made this?", "Nork egin du hau?");
         pluralEntries.put(
-                new PluralId("+%# sec", "+%# sec"),
-                new String[] {
-                        "+%# segundo",
-                        "+%# segundo",
-                });
+            new PluralId("+%# sec", "+%# sec"),
+            new String[] {
+                "+%# segundo",
+                "+%# segundo",
+            }
+        );
         pluralEntries.put(
-                new PluralId("1 remaining.", "%# remaining."),
-                new String[] {
-                        "1 faltan.",
-                        "%# faltan.",
-                });
+            new PluralId("1 remaining.", "%# remaining."),
+            new String[] {
+                "1 faltan.",
+                "%# falta.",
+            }
+        );
         pluralEntries.put(
-                new PluralId("ignore-close-call", "Finish a level with less than %# seconds left."),
-                new String[] {
-                        "-",
-                        "Buka ezazu maila bat %# baino segundo gutxiago falta direlarik.",
-                });
+            new PluralId("ignore-close-call", "Finish a level with less than %# seconds left."),
+            new String[] {
+                "ignore-close-call",
+                "Buka ezazu maila bat %# baino segundo gutxiago falta direlarik.",
+            }
+        );
         pluralEntries.put(
-                new PluralId("ignore-collect", "Collect %# stars."),
-                new String[] {
-                        "-",
-                        "Lortu %# izar.",
-                });
+            new PluralId("ignore-collect", "Collect %# stars."),
+            new String[] {
+                "ignore-collect",
+                "Lortu %# izar.",
+            }
+        );
         pluralEntries.put(
-                new PluralId("ignore-creative", "Create %# different burgers in the practice area."),
-                new String[] {
-                        "-",
-                        "Sortu %# burger desberdin praktika txokoan.",
-                });
+            new PluralId("ignore-creative", "Create %# different burgers in the practice area."),
+            new String[] {
+                "ignore-creative",
+                "Sortu %# burger desberdin praktika txokoan.",
+            }
+        );
         pluralEntries.put(
-                new PluralId("ignore-evening", "Start a game between 7PM and 11PM for %# days."),
-                new String[] {
-                        "-",
-                        "Hasi jokoan 19:00ak eta 23:00ak bitartean %# egunez.",
-                });
+            new PluralId("ignore-evening", "Start a game between 7PM and 11PM for %# days."),
+            new String[] {
+                "ignore-evening",
+                "Hasi jokoan arratsaldeko 19:00ak eta 23:00ak bitartean\n%# egunez.",
+            }
+        );
         pluralEntries.put(
-                new PluralId("ignore-fan", "Play %# levels."),
-                new String[] {
-                        "-",
-                        "Jostatu %# mailatan.",
-                });
+            new PluralId("ignore-fan", "Play %# levels."),
+            new String[] {
+                "ignore-fan",
+                "Jostatu %# mailatan.",
+            }
+        );
         pluralEntries.put(
-                new PluralId("ignore-morning", "Start a game between 7AM and 10AM for %# days."),
-                new String[] {
-                        "-",
-                        "Hasi jokoan 7:00ak eta 10:00ak bitartean %# egunez.",
-                });
+            new PluralId("ignore-morning", "Start a game between 7AM and 10AM for %# days."),
+            new String[] {
+                "ignore-morning",
+                "Hasi jokoan 7:00ak eta 10:00akak bitartean %# egunez.",
+            }
+        );
         pluralEntries.put(
-                new PluralId("ignore-n-burgers", "Serve %# burgers."),
-                new String[] {
-                        "-",
-                        "Zerbitzatu %# burger.",
-                });
+            new PluralId("ignore-n-burgers", "Serve %# burgers."),
+            new String[] {
+                "ignore-n-burgers",
+                "Zerbitzatu %# burger.",
+            }
+        );
         pluralEntries.put(
-                new PluralId("ignore-practice", "Play %# levels to unlock the practice area."),
-                new String[] {
-                        "-",
-                        "Jokatu %# maila praktika txokoa irekitzeko.",
-                });
+            new PluralId("ignore-practice", "Play %# levels to unlock the practice area."),
+            new String[] {
+                "ignore-practice",
+                "Jokatu %# maila praktika txokoa irekitzeko.",
+            }
+        );
     }
 
     @Override
     public int plural(int n) {
-        return (n > 1) ? 1 : 0;
+        return (n != 1) ? 1 : 0;
     }
 }
